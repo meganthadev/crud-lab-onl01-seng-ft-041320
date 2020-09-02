21056e38f01ec1730c67313ffb44ff3c7f0f2556
Megan Horsfield <meganthadev@gmail.com>
2020-09-02 07:17:11 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RestaurantsContainer from './containers/RestaurantsContainer';
+import ReviewsContainer from './containers/ReviewsContainer';
 
 class App extends Component {
   render() {
