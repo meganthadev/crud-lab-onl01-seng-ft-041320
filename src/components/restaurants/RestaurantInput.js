8b40a4746fdb8d7d18c9ebd4557f890b3a8f3778
Megan Horsfield <meganthadev@gmail.com>
2020-09-02 07:17:48 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -5,6 +5,11 @@ class RestaurantInput extends Component {
    constructor(props) {
     super(props);
     
+    this.state = {
+      text: ''
+    };
+  };
+  
   render() {
     return (
       <div>
