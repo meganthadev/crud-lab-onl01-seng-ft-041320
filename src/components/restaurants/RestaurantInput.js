12ae65923b61c0d86d563d832f71adc42ac6aef7
Megan Horsfield <meganthadev@gmail.com>
2020-09-02 07:17:34 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  
+   constructor(props) {
+    super(props);
+    
   render() {
     return (
       <div>
